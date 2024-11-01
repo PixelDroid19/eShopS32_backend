479a2961ad288b864ce26f2ac80d550cd5e5b45c
AXG25 <axiever.guejia@gmail.com>
2024-11-01 12:50:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ const publicStoreRoutes = require('./routes/publicStoreRoutes');
 const app = express();
 const PORT = 3000;
 
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
 // Middleware
 app.use(cors());
 app.use(bodyParser.json({limit: '50mb'}));
